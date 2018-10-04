ff6b7b2927876860197c2d958c98152fdaf11dd8
jberry93 <jeffreyberry93@gmail.com>
2018-10-03 22:15:39 -0700

    feat(redux-devtools): connect devtools to store

--- src/store/configureStore.ts
+++ src/store/configureStore.ts
@@ -1,6 +1,8 @@
 import {createStore} from 'redux';
+import {composeWithDevTools} from 'redux-devtools-extension';
 import {rootReducer} from './reducers';
 
 export const store = createStore(
     rootReducer,
+    composeWithDevTools()
 );
