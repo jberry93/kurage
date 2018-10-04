9661b2e9d6b2de04f5adfc62c413fbd6f79d1902
jberry93 <jeffreyberry93@gmail.com>
2018-10-03 22:15:09 -0700

    fix(mapToProps): successfully map state and actions to props in CounterPage class component

--- src/pages/Counter.tsx
+++ src/pages/Counter.tsx
@@ -3,32 +3,34 @@ import {connect} from 'react-redux';
 import {withRouter, RouteComponentProps} from 'react-router-dom';
 
 interface CounterProps extends RouteComponentProps {
-    count: string;
-    onIncrement: string;
-    onDecrement: string;
+    count: number;
+    onIncrement: Function;
+    onDecrement: Function;
 };
 interface CounterState {};
 
 class CounterPage extends React.Component<CounterProps, CounterState> {
+    public constructor(props: CounterProps, state: CounterState) {
+        super(props);
+    }
+
     public render(): JSX.Element {
         const {count, onIncrement, onDecrement} = this.props;
         return (
             <div className="CounterWrapper">
                 <h1>Count With Redux!</h1>
-                <p>{{count}}</p>
-                <button onClick={() => onIncrement}>+</button>
-                <button onClick={() => onDecrement}>-</button>
+                <p>{count}</p>
+                <button onClick={() => onIncrement()}>+</button>
+                <button onClick={() => onDecrement()}>-</button>
             </div>
         );
     }
 }
 
-const mapStateToProps = state => ({
-    count: state,
-    onIncrement: state.dispatch({type: 'INCREMENT'}),
-    onDecrement: state.dispatch({type: 'DECREMENT'}),    
-});
-
-const mapDispatchToProps = () => ({})
+const mapStateToProps = ({counter: {count}}: {counter: {count: number}}): {count: number} => ({count});
+const mapDispatchToProps = (dispatch): {onIncrement: Function, onDecrement: Function} => ({
+    onIncrement: () => dispatch({type: 'INCREMENT'}),
+    onDecrement: () => dispatch({type: 'DECREMENT'}),
+})
 
 export const Counter = withRouter(connect(mapStateToProps, mapDispatchToProps)(CounterPage));
